0d7525bcad5a1edfcf7faab14e0e3bf046a3a008
HishamAC1 <hishamac613@yahoo.com>
2026-05-07 18:31:49 -0500

    Part 1 complete

--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -1,24 +1,28 @@
-
 import java.util.Scanner;
 
 public class GiftTax {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Value of the gift?");
-        int value = Integer.valueOf(scan.nextLine());
-        if (value < 5000) {
+        int gift = Integer.valueOf(scan.nextLine());
+        
+        if (gift < 5000) {
             System.out.println("No tax!");
-        } else if (value >= 5000 && value <= 25000) {
-            System.out.println("Tax: 100");
-        } else if (value >= 25000 && value <= 55000) {
-            System.out.println("Tax: 1700");
-        } else if (value >= 55000 && value <= 200000) {
-            System.out.println("Tax: 4700");
-        } else if (value >= 200000 && value <= 1000000){
-            System.out.println("Tax: 22100");
+        } else if (gift <= 25000) {
+            double tax = 100 + (gift - 5000) * 0.08;
+            System.out.println("Tax: " + tax);
+        } else if (gift <= 55000) {
+            double tax = 1700 + (gift - 25000) * 0.10;
+            System.out.println("Tax: " + tax);
+        } else if (gift <= 200000) {
+            double tax = 4700 + (gift - 55000) * 0.12;
+            System.out.println("Tax: " + tax);
+        } else if (gift <= 1000000) {
+            double tax = 22100 + (gift - 200000) * 0.15;
+            System.out.println("Tax: " + tax);
         } else {
-            System.out.println("Tax: 142100");
+            double tax = 142100 + (gift - 1000000) * 0.17;
+            System.out.println("Tax: " + tax);
         }
     }
 }
