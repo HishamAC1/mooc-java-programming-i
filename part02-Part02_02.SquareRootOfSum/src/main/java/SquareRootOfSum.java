c1b9167f161c4af80a6c1f6061f7d16c5af47126
HishamAC1 <hishamac613@yahoo.com>
2026-05-07 19:39:57 -0500

    Part 2 started, recurring problems and patterns complete

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,9 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numberone = Integer.valueOf(scanner.nextLine());
+        int numbertwo = Integer.valueOf(scanner.nextLine());
+        double squareRoot = Math.sqrt(numberone + numbertwo);
+        System.out.println(squareRoot);
     }
 }
