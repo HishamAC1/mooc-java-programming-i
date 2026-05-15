f50ab6b4d5cdc4049039e3e23cf1be577b8d2685
HishamAC1 <hishamac613@yahoo.com>
2026-05-15 16:05:35 -0500

    Part 2 - More Loops complete, started Methods section, stopping at From Parameter to One

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,12 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int fact = 1;
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        for (int i = 1; i <= number; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial: " + fact);
     }
 }
