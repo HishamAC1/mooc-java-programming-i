e3990f83c39f2e32fe1bb10ca9205595e22f8f3c
HishamAC1 <hishamac613@yahoo.com>
2026-05-19 18:22:03 -0500

    Part 2 - Methods section progress, stopping at Methods can return values

--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -3,8 +3,16 @@
 public class FromParameterToOne {
 
     public static void main(String[] args) {
-
+        printFromNumberToOne(5);
 
     }
+    private static void printFromNumberToOne(int number) {
+        while (number >= 1 ){
+            System.out.println(number);
+            number --;
+        }
+        
+    }
+    
 
 }
