964fa81cfe6bc14d2e713b2d9561092b4857e19d
HishamAC1 <hishamac613@yahoo.com>
2026-05-21 20:01:52 -0500

    Part 2 - Methods section progress, stopping at StarSign

--- part02-Part02_28.Word/src/main/java/Word.java
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -5,7 +5,11 @@ public class Word {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String potato = word();
+        System.out.println(potato);
 
     }
-
+    public static String word(){
+        return "potato";
+    }
 }
